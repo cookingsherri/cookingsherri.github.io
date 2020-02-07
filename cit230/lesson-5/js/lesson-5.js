406ed1b7ba85dc5c3813f13a0b64348616b21427
Sherri Walter <cookingsherri@byui.edu>
2020-02-07 18:11:54 -0500

    ch cha changes

---
+++ cit230/lesson-5/js/lesson-5.js
@@ -0,0 +1,14 @@
+const hambutton = document.querySelector(".ham");
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+  document.querySelector(".navigation").classList.toggle("responsive");
+}
+var monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var dayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var today = new Date();
+var dayOfWeek = dayArray[today.getDay()];
+var monthName = monthArray[today.getMonth()];
+var currentDate = dayOfWeek + ", " + today.getDate() + " " + monthName + " " + (today.getYear() + 1900);
+document.getElementById("currentDate").innerHTML = currentDate;
+

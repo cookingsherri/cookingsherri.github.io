5fa33dcb108dd780735811082b5198182ccddfe9
Sherri Walter <cookingsherri@byui.edu>
2020-02-10 16:59:07 -0500

    displaybanner

--- cit230/lesson-5/js/bannerad.js
+++ cit230/lesson-5/js/bannerad.js
@@ -1,8 +1,12 @@
-function displayBanner(){
+
 	let date = new Date();
-	let day = date.getDay();
-	if(day==5){
+    let day = date.getDay();
+    
+	if(day==1){
+        console.log(5);
 		document.getElementById("banner").style.display="block";
     }
-  
+    
+
+ 
     

73a3756a129deae0790efc93ea167abd7ad3cd5f
Sherri Walter <cookingsherri@byui.edu>
2020-03-10 21:12:48 -0400

    mobile responsive home page

--- cit230/lesson-9/home.js
+++ cit230/lesson-9/home.js
@@ -8,39 +8,43 @@ fetch(requestURL)
   .then(function(jsonObject) {
     //console.table(jsonObject);
     const towns = jsonObject["towns"];
-    for (let i = 0; i < towns.length; i++) {
+    
         towns.forEach(city => {
+          if (city.name == "Preston" || city.name =="Fish Haven" || city.name =="Soda Springs") {
+
+          
         
-      let card = document.createElement("section");
-      let name = document.createElement("h2");
-      let motto = document.createElement("h3");
-      let yearFounded = document.createElement("p");
-      let currentPopulation = document.createElement("p");
-      let averageRainfall = document.createElement("p");
-      let photo = document.createElement("img");
+          let card = document.createElement("section");
+          let div = document.createElement("div");
+          let div2 = document.createElement("div");
+          let name = document.createElement("h2");
+          let motto = document.createElement("h3");
+          let yearFounded = document.createElement("p");
+          let currentPopulation = document.createElement("p");
+          let averageRainfall = document.createElement("p");
+          let photo = document.createElement("img");
 
-switch (city.name) {
-    case 'Fish Haven':
-    case 'Preston':
-    case 'Soda Springs':
-     name.textContent = city.name;
-      motto.textContent = city.motto;
-      yearFounded.textContent = "Year Founded:" + city.yearFounded;
-      currentPopulation.textContent = "Population:" + city.currentPopulation;
-      averageRainfall.textContent = "Average Rainfall:" + city.averageRainfall;
-      
-      image.setAttribute('src', city.photo);
-      image.setAttribute('alt',  "Photo of " + city.name);
 
-      card.appendChild(name);
-      card.appendChild(motto);
-      card.appendChild(yearFounded);
-      card.appendChild(currentPopulation);
-      card.appendChild(averageRainfall);
-      card.appendChild(photo);
-      
+          name.textContent = city.name;
+          motto.textContent = city.motto;
+          yearFounded.textContent = "Year Founded:" + city.yearFounded;
+          currentPopulation.textContent = "Population:" + city.currentPopulation;
+          averageRainfall.textContent = "Average Rainfall:" + city.averageRainfall;
 
-      document.querySelector("div.cards").appendChild(card);
-    }
-  });
-});
+          photo.setAttribute('src', "images/" + city.photo);
+          photo.setAttribute('alt',  "Photo of " + city.name);
+
+          div.appendChild(name);
+          div.appendChild(motto);
+          div.appendChild(yearFounded);
+          div.appendChild(currentPopulation);
+          div.appendChild(averageRainfall);
+          card.appendChild(div);
+          div2.appendChild(photo);
+          card.appendChild(div2);
+            
+
+           document.querySelector("div.cards").appendChild(card);
+          }}
+  );
+    });

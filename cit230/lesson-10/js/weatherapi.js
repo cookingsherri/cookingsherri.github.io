825796a5af602a91802374631e9d86984ded08e7
Sherri Walter <cookingsherri@byui.edu>
2020-03-12 22:00:24 -0400

    console and imperial

--- cit230/lesson-10/js/weatherapi.js
+++ cit230/lesson-10/js/weatherapi.js
@@ -1,8 +1,7 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=1a20634257e80075a78ee9798275cb13";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&&APPID=1a20634257e80075a78ee9798275cb13";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject);
     document.getElementById('current-temp').textContent = jsObject.main.temp;
     const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
 const desc = jsObject.weather[0].description;  // note how we reference the weather array

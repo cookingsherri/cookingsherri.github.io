6aee3298fcc639c300f2ec7cdf91eb5accaffe1b
Sherri Walter <cookingsherri@byui.edu>
2020-03-14 17:13:50 -0400

    forecast

--- cit230/lesson-10/js/weatherapi.js
+++ cit230/lesson-10/js/weatherapi.js
@@ -2,6 +2,7 @@ const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
+    console.log(jsObject);
     document.getElementById('current-temp').textContent = jsObject.main.temp;
     const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
 const desc = jsObject.weather[0].description;  // note how we reference the weather array

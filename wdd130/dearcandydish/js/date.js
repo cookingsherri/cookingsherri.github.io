e8e4d0afeed2f2dd4377f59ebb9689a5e44da170
Sherri Walter <cookingsherri@byui.edu>
2020-03-14 23:22:04 -0400

    Final Website Progress

---
+++ wdd130/dearcandydish/js/date.js
@@ -0,0 +1,10 @@
+
+var monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var dayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var today = new Date();
+var dayOfWeek = dayArray[today.getDay()];
+var monthName = monthArray[today.getMonth()];
+var currentDate = dayOfWeek + ", " + today.getDate() + " " + monthName + " " + (today.getYear() + 1900);
+document.getElementById("currentDate").innerHTML = currentDate;
+
+

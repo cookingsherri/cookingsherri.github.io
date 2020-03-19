2ff37f2136781fea4aca6065da53428e4154bdc7
Sherri Walter <cookingsherri@byui.edu>
2020-03-19 19:10:33 -0400

    js updates

--- cit230/lesson-11/js/fish-haven-weather.js
+++ cit230/lesson-11/js/fish-haven-weather.js
@@ -1,27 +1,24 @@
-const papiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&&APPID=1a20634257e80075a78ee9798275cb13";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604045&units=imperial&&APPID=1a20634257e80075a78ee9798275cb13";
 
-
-fetch(papiURL)
-
+fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
-        console.log(jsObject);
+        // console.log(jsObject);
         document.getElementById('current').textContent = jsObject.weather[0].description;
         document.getElementById('highTemp').textContent = jsObject.main.temp_max.toFixed(0);
         document.getElementById('humidity').textContent = jsObject.main.humidity;
         document.getElementById('windSpeed').textContent = jsObject.wind.speed.toFixed(0);
         document.getElementById('windChill').textContent = calculateWindChill(jsObject.main.temp_max.toFixed(0), jsObject.wind.speed.toFixed(0));
     });
+
+   function calculateWindChill(t, s) {
     
-    function calculateWindChill(t, s) {
-    
-        if (t <= 50 && s > 3) {
-            var w = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
-            results = w.toFixed(0);
-        }
-        
-        else results = "N/A";
-        
-        return results;
+    if (t <= 50 && s > 3) {
+        var w = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+        results = w.toFixed(0);
     }
     
+    else results = "N/A";
+    
+    return results;
+}
--- cit230/lesson-11/js/soda-springs-weather.js
+++ cit230/lesson-11/js/soda-springs-weather.js
@@ -1,8 +1,6 @@
-const papiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&&APPID=1a20634257e80075a78ee9798275cb13";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5607916&units=imperial&&APPID=1a20634257e80075a78ee9798275cb13";
 
-
-fetch(papiURL)
-
+fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
@@ -12,16 +10,15 @@ fetch(papiURL)
         document.getElementById('windSpeed').textContent = jsObject.wind.speed.toFixed(0);
         document.getElementById('windChill').textContent = calculateWindChill(jsObject.main.temp_max.toFixed(0), jsObject.wind.speed.toFixed(0));
     });
+
+   function calculateWindChill(t, s) {
     
-    function calculateWindChill(t, s) {
-    
-        if (t <= 50 && s > 3) {
-            var w = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
-            results = w.toFixed(0);
-        }
-        
-        else results = "N/A";
-        
-        return results;
+    if (t <= 50 && s > 3) {
+        var w = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+        results = w.toFixed(0);
     }
     
+    else results = "N/A";
+    
+    return results;
+}
